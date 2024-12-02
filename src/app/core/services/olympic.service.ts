ce5e0e70d8b90fdd2e0dbbe2c05ab4984be05fe1
sihem taguelmimt <t.sihem-26@outlook.com>
2024-12-02 11:47:07 +0100

    error handling

--- src/app/core/services/olympic.service.ts
+++ src/app/core/services/olympic.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { BehaviorSubject, of } from 'rxjs';
-import { catchError, finalize, tap } from 'rxjs/operators';
+import { BehaviorSubject, of, Subject } from 'rxjs';
+import { catchError, finalize, takeUntil, tap } from 'rxjs/operators';
 import {Olympic} from 'src/app/core/models/Olympic';
 
 @Injectable({
@@ -10,31 +10,38 @@ import {Olympic} from 'src/app/core/models/Olympic';
 export class OlympicService {
   private olympicUrl = './assets/mock/olympic.json';
   private olympics$ = new BehaviorSubject<Olympic[] | null>(null);
-  private isLoading = false;
+  private destroy$ = new Subject<void>();
+  //private isLoading = false;
 
   constructor(private http: HttpClient) {}
 
   //Charger et émettre les données depuis le fichier JSON via le BehaviorSubject
   loadInitialData() {
-    if(this.isLoading){
+    /*if(this.isLoading){
       return of();
     }
-    this.isLoading = true;
+    this.isLoading = true;*/
     return this.http.get<Olympic[]>(this.olympicUrl).pipe(
       tap((value) => this.olympics$.next(value)),
+      takeUntil(this.destroy$),
       catchError((error, caught) => {
         console.error(error);
         this.olympics$.next(null);
         return of(null);
       }),
-      finalize(()=>{
+      /*finalize(()=>{
         this.isLoading = false;
-      })
+      })*/
     );
   }
 
   //Méthode pour s'abonner au BehaviorSubject
   getOlympics() {
     return this.olympics$.asObservable();
+  }
+
+  ngOnDestroy() : void{
+    this.destroy$.next();
+    this.destroy$.complete();
   }
 }
